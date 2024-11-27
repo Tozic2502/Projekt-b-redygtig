6f6b90816d4ce30b0b80576509a99238d2dad9ad
najaf <najafj@me.com>
2024-11-27 08:39:11 +0100

    Final UI design push

--- src/main/java/org/example/projektbaedygtig/Controller.java
+++ src/main/java/org/example/projektbaedygtig/Controller.java
@@ -2,12 +2,20 @@ package org.example.projektbaedygtig;
 
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class Controller {
 
+@FXML
+private ChoiceBox<String> choiceBoxSite;
 
-    @FXML
-    protected void onHelloButtonClick() {
+@FXML
+private ChoiceBox<String> choiceBoxMonth;
 
+
+@FXML
+    public void initialize() {
+        choiceBoxSite.setValue("Site");
+        choiceBoxMonth.setValue("January");
     }
 }
