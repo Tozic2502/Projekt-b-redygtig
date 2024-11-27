e9088f0358ae39c530d92900c9c0b2cea7269fa1
Tozic <Mikcollatz@gmail.com>
2024-11-27 09:35:28 +0100

    Button Start

--- src/main/java/org/example/projektbaedygtig/Controller.java
+++ src/main/java/org/example/projektbaedygtig/Controller.java
@@ -3,19 +3,93 @@ package org.example.projektbaedygtig;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.chart.LineChart;
 
 public class Controller {
 
-@FXML
-private ChoiceBox<String> choiceBoxSite;
+    @FXML
+    private ChoiceBox<String> choiceBoxSite;
+    @FXML
+    private LineChart monthChart, yearChart, dayChart;
 
-@FXML
-private ChoiceBox<String> choiceBoxMonth;
-
-
-@FXML
+    @FXML
     public void initialize() {
         choiceBoxSite.setValue("Site");
         choiceBoxMonth.setValue("January");
+        OnButtonClickReset();
+        OnButtonClickReset();
     }
+
+    @FXML
+    private ChoiceBox<String> choiceBoxMonth;
+
+    @FXML
+    protected void OnButtonClickReset() {
+
+        monthChart.getData().clear();
+        yearChart.getData();
+        dayChart.getData().clear();
+        if (monthChart.isVisible()) {
+            monthChart.setVisible(false);
+
+        } else if (dayChart.isVisible()) {
+            dayChart.setVisible(false);
+
+        } else if (!yearChart.isVisible()) {
+            yearChart.setVisible(true);
+        }
+
+    }
+
+    @FXML
+    protected void OnButtonClickPick() {
+        String selectedMonth = choiceBoxMonth.getValue();
+
+
+            if (selectedMonth != null) {
+                switch (selectedMonth) {
+                    case "January":
+                        monthChart.getData().clear();
+                        monthChart.setVisible(true);
+                        monthChart.getData().addAll();
+                        break;
+                    case "February":
+
+                        break;
+                    case "March":
+
+                        break;
+                    case "April":
+
+                        break;
+                    case "May":
+
+                        break;
+                    case "June":
+
+                        break;
+                    case "July":
+
+                        break;
+                    case "August":
+
+                        break;
+                    case "September":
+
+                        break;
+                    case "October":
+
+                        break;
+                    case "November":
+
+                        break;
+                    case "December":
+
+                        break;
+
+                }
+            }
+
+    }
+
 }
