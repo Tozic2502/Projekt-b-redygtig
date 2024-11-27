8286b047c938555897b84a1d33d0284c3015217b
Theis <theislausten02@gmail.com>
2024-11-27 10:27:16 +0100

    Final month Choicebox

--- src/main/java/org/example/projektbaedygtig/Controller.java
+++ src/main/java/org/example/projektbaedygtig/Controller.java
@@ -12,25 +12,19 @@ public class Controller {
 
     @FXML
     public void initialize() {
+
+
         /**
          * Populate months
          */
         ChoiceBoxMonth.getItems().addAll("January","February", "March","April", "May",
                 "June", "July", "August", "September", "October", "November", "December" );
 
+
         /**
          *  A default value/month is set
          */
         ChoiceBoxMonth.setValue("January");
 
-        /**
-         * Add a listener for month selection
-         */
-        ChoiceBoxMonth.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println("Selected Month" + newValue);
-        });
+        };
     }
-
-    public void OnButtonClickReset(ActionEvent actionEvent) {
-    }
-}
